6a223191eee30693f3e954cf2a43fe5c3ca21fcf
Mstcimt <Michael.Stricker@cimt-ag.de>
2017-06-09 11:01:31 +0200

    mysql repo

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Confirm the path values given below correspond to your installation
 
-MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
-HADOOP=/opt/cloudera/parcels/CDH/bin
+MR=/opt/cloudera/parcels/CDH-5.8.3-1.cdh5.8.3.p0.2/lib/hadoop-0.20-mapreduce
+HADOOP=/usr/bin
+USER=/user/ec2-user
 
 # Mark start of the loop
 echo Testing loop started on `date`
@@ -21,12 +22,19 @@ do
 
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
+		 
+		 echo "mapreduce.job.maps=$i"
+		 echo "mapreduce.job.reduces=$j"
+		 echo "mapreduce.map.memory.mb=$k"
+		 echo "mapreduce.map.java.opts.max.heap=$MAP_MB"
+
+		 echo "mapreduce.reduce.java.opts.max.heap=$RED_MB"
 
         time ${HADOOP}/hadoop jar ${MR}/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
-                     51200000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
+                     51200000 $USER/results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
        time ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
@@ -35,11 +43,11 @@ do
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      -Dmapreduce.reduce.memory.mb=$k \
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
-	             /results/tg-10GB-${i}-${j}-${k}  \
-                     /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
+	             $USER/results/tg-10GB-${i}-${j}-${k}  \
+                     $USER/results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+        $HADOOP/hadoop fs -rm -r -skipTrash $USER/results/tg-10GB-${i}-${j}-${k}                         
+        $HADOOP/hadoop fs -rm -r -skipTrash $USER/results/ts-10GB-${i}-${j}-${k}                 
       done
    done
 done
